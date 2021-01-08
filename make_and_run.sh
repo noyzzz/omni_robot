40381e71b5a80b03c689168497d8cbf2fa6ae1ee
Navid Mahdian <mahdian.navid@gmail.com>
2021-01-08 13:25:49 +0330

    setup added

---
+++ make_and_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p ./catkin_ws/src
+cd ./catkin_ws
+catkin_make
+cd src
+catkin_create_pkg rob_pkg
+cd .. && catkin_make
+cd src/rob_pkg && rm *
+git clone https://github.com/noyzzz/hello-world.git
+cd ../.. && source devel/setup.bash
+roslaunch -v rob_pkg spawn_urdf.launch
+
